1871007ffdd14259137db9f7d52f9c15e6acd132
Serhii Blyzniuk <bliznyuk.sergey@gmail.com>
2022-01-25 00:31:16 +0200

    Initial commit

---
+++ src/main/java/autotasks/CarouselRun.java
@@ -0,0 +1,13 @@
+package autotasks;
+
+public class CarouselRun {
+
+    public int next() {
+       throw new UnsupportedOperationException();
+    }
+
+    public boolean isFinished() {
+        throw new UnsupportedOperationException();
+    }
+
+}
---
+++ src/main/java/autotasks/DecrementingCarousel.java
@@ -0,0 +1,15 @@
+package autotasks;
+
+public class DecrementingCarousel {
+    public DecrementingCarousel(int capacity) {
+
+    }
+
+    public boolean addElement(int element){
+        throw new UnsupportedOperationException();
+    }
+
+    public CarouselRun run(){
+       throw new UnsupportedOperationException();
+    }
+}
