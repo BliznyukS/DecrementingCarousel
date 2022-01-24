db3c00510557c36eb251738251e446731f43d2f0
Serhii Blyzniuk <bliznyuk.sergey@gmail.com>
2022-01-25 01:09:00 +0200

    add comments with tasks for methods

--- src/main/java/autotasks/CarouselRun.java
+++ src/main/java/autotasks/CarouselRun.java
@@ -3,10 +3,19 @@ package autotasks;
 public class CarouselRun {
 
     public int next() {
+        //  Возвращает текущее значение текущего элемента, затем уменьшает текущий элемент на единицу и переключается на следующий элемент в порядке добавления.
+        //  Пропускает нулевые элементы. Когда больше нет элементов для уменьшения, возвращает -1
        throw new UnsupportedOperationException();
     }
 
     public boolean isFinished() {
+        // Когда больше нету елементов дл яуменьшения true, иначе false
+
+//        if (){
+//            return true;
+//        } else {
+//            return false;
+//        }
         throw new UnsupportedOperationException();
     }
 
--- src/main/java/autotasks/DecrementingCarousel.java
+++ src/main/java/autotasks/DecrementingCarousel.java
@@ -6,10 +6,15 @@ public class DecrementingCarousel {
     }
 
     public boolean addElement(int element){
+        // Добавляет элемент. Если элемент отрицателен или равен нулю, не добавляет его.
+        // Если контейнер заполнен, не добавляет элемент. Если метод run был вызван для создания CarouselRun, не добавляет элемент.
+        // Если элемент добавлен успешно, возвращает true. В противном случае – false.
         throw new UnsupportedOperationException();
     }
 
     public CarouselRun run(){
+        //     Возвращает CarouselRun для перебора элементов.
+        //     Если метод run уже был вызван ранее, он должен вернуть null: DecrementingCarousel может генерировать только один объект CarouselRun.
        throw new UnsupportedOperationException();
     }
 }
