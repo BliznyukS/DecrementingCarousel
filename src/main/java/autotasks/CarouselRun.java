e486a016f479e0ce46aab56a5c45c822364f3041
Serhii Blyzniuk <bliznyuk.sergey@gmail.com>
2022-01-25 21:41:35 +0200

    add run(), addElement(), isFinished(), CarouselRun(), DecrementingCarousel()

--- src/main/java/autotasks/CarouselRun.java
+++ src/main/java/autotasks/CarouselRun.java
@@ -1,22 +1,53 @@
 package autotasks;
 
+import java.util.LinkedList;
+
+
 public class CarouselRun {
+
+    private int amountOfNotZeroElements; //could be atomicInt
+    private LinkedList<Integer> carusel;
+
+    public int getAmountOfNotZeroElements() {
+        return amountOfNotZeroElements;
+    }
+
+    public void setAmountOfNotZeroElements(int amountOfNotZeroElements) {
+        this.amountOfNotZeroElements = amountOfNotZeroElements;
+    }
+
+    public LinkedList<Integer> getCarusel() {
+        return carusel;
+    }
+
+    public void setCarusel(LinkedList<Integer> carusel) {
+        this.carusel = carusel;
+    }
+
+
+
+    public CarouselRun() {
+        this.carusel = new LinkedList();
+    }
 
     public int next() {
         //  Возвращает текущее значение текущего элемента, затем уменьшает текущий элемент на единицу и переключается на следующий элемент в порядке добавления.
         //  Пропускает нулевые элементы. Когда больше нет элементов для уменьшения, возвращает -1
-       throw new UnsupportedOperationException();
+       //throw new UnsupportedOperationException();
+        carusel.get(0);
+        return  -1;
+
     }
 
     public boolean isFinished() {
-        // Когда больше нету елементов дл яуменьшения true, иначе false
+        // Когда больше нету елементов для уменьшения true, иначе false
 
-//        if (){
-//            return true;
-//        } else {
-//            return false;
-//        }
-        throw new UnsupportedOperationException();
+        if (amountOfNotZeroElements > 0){
+            return false;
+        } else {
+            return true;
+        }
+       // throw new UnsupportedOperationException();
     }
 
 }
--- src/main/java/autotasks/DecrementingCarousel.java
+++ src/main/java/autotasks/DecrementingCarousel.java
@@ -1,20 +1,53 @@
 package autotasks;
 
 public class DecrementingCarousel {
+
+//    private boolean flag;
+
+    private int currentElementIndexForAdd;// do not need for LinkedList
+    private CarouselRun carouselRun;
+    private boolean isRunning;
+    private int capacity;
+
+
     public DecrementingCarousel(int capacity) {
+        this.isRunning = false;
+        this.capacity = capacity;
+        this.carouselRun = new CarouselRun();
+        // init array
 
     }
 
-    public boolean addElement(int element){
+    public boolean addElement(int element) {
+        if (isRunning) {
+            //throw new UnsupportedOperationException();
+            return false;
+        }
+
+        if (element > 0 && capacity > 0) {
+            carouselRun.setAmountOfNotZeroElements(carouselRun.getAmountOfNotZeroElements() + 1);
+            carouselRun.getCarusel().add(element);
+            capacity = capacity - 1;
+            return true;
+        } else {
+            return false;
+        }
         // Добавляет элемент. Если элемент отрицателен или равен нулю, не добавляет его.
         // Если контейнер заполнен, не добавляет элемент. Если метод run был вызван для создания CarouselRun, не добавляет элемент.
         // Если элемент добавлен успешно, возвращает true. В противном случае – false.
-        throw new UnsupportedOperationException();
+        //throw new UnsupportedOperationException();
     }
 
-    public CarouselRun run(){
+    public CarouselRun run() {
         //     Возвращает CarouselRun для перебора элементов.
         //     Если метод run уже был вызван ранее, он должен вернуть null: DecrementingCarousel может генерировать только один объект CarouselRun.
-       throw new UnsupportedOperationException();
+
+        if (isRunning) {
+            return null;
+        } else {
+            isRunning = true;
+            return carouselRun;
+        }
+        //throw new UnsupportedOperationException();
     }
 }
